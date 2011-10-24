94bc3f25ad6b93005a6dbc26c6bd000bd9b8c1fd
Aaron Gallagher <habnabit@gmail.com>
2011-10-23 21:29:24 -0700

    SQLAlchemy metaprogramming to reduce the code down.

--- shurts.py
+++ shurts.py
@@ -30,11 +30,26 @@ db = SQLAlchemy(app)
 
 oid = OpenID(app)
 
-class Kwinit(object):
-    def __init__(self, **kwargs):
-        for kv in kwargs.iteritems():
-            setattr(self, *kv)
-        super(Kwinit, self).__init__()
+def rel_generator(parent, plural, singular=None):
+    singular = singular or parent.__name__.lower()
+    def deco(cls):
+        lower_child = cls.__name__.lower()
+        child_fk = db.Column(db.Integer(), db.ForeignKey(cls.id), primary_key=True)
+        generated_type = type(parent)(
+            cls.__name__ + parent.__name__,
+            (parent,),
+            {
+                '__tablename__': '%s_%s' % (lower_child, plural),
+                '__mapper_args__': dict(polymorphic_identity=lower_child),
+                singular + '_id': db.Column(db.Integer(), db.ForeignKey(parent.id), primary_key=True),
+                lower_child + '_id': child_fk,
+                lower_child: db.relationship(cls, backref=plural)
+            }
+        )
+        setattr(cls, 'has_' + singular, db.column_property(db.exists(['*']).where(cls.id == child_fk)))
+        setattr(cls, parent.__name__, generated_type)
+        return cls
+    return deco
 
 class Editor(db.Model):
     __tablename__ = 'editors'
@@ -52,6 +67,9 @@ class Note(db.Model):
     def formatted(self):
         return genshi.Markup(markdown.markdown(self.note))
 
+with_notes = rel_generator(Note, 'notes')
+
+@with_notes
 class Photo(db.Model):
     __tablename__ = 'photos'
     id = db.Column(db.Integer(), primary_key=True)
@@ -64,35 +82,32 @@ class Photo(db.Model):
     def url(self):
         return photo_set.url(self.filename)
 
-class PhotoNote(Note):
-    __tablename__ = 'photo_notes'
-    __mapper_args__ = dict(polymorphic_identity='photo')
-    note_id = db.Column(db.Integer(), db.ForeignKey(Note.id), primary_key=True)
-    photo_id = db.Column(db.Integer(), db.ForeignKey(Photo.id), primary_key=True)
-    photo = db.relationship(Photo, backref='notes')
+with_photos = rel_generator(Photo, 'photos')
 
+class PendingPhoto(db.Model):
+    __tablename__ = 'pending_photos'
+    id = db.Column(db.Integer(), primary_key=True)
+    editor_id = db.Column(db.String(), db.ForeignKey(Editor.openid))
+    editor = db.relationship(Editor, backref='pending_photos')
+    when = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.now)
+    key = db.Column(db.String(), nullable=False)
+    type = db.Column(db.String(), nullable=False)
+    __mapper_args__ = dict(polymorphic_on=type)
+
+with_pending_photos = rel_generator(PendingPhoto, 'pending_photos', singular='pending_photo')
+
+@with_notes
+@with_photos
+@with_pending_photos
 class Shirt(db.Model):
     __tablename__ = 'shirts'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
     acquired = db.Column(db.Date())
 
-class ShirtNote(Note):
-    __tablename__ = 'shirt_notes'
-    __mapper_args__ = dict(polymorphic_identity='shirt')
-    note_id = db.Column(db.Integer(), db.ForeignKey(Note.id), primary_key=True)
-    shirt_id = db.Column(db.Integer(), db.ForeignKey(Shirt.id), primary_key=True)
-    shirt = db.relationship(Shirt, backref='notes')
-
-class ShirtPhoto(Photo):
-    __tablename__ = 'shirt_photos'
-    __mapper_args__ = dict(polymorphic_identity='shirt')
-    photo_id = db.Column(db.Integer(), db.ForeignKey(Photo.id), primary_key=True)
-    shirt_id = db.Column(db.Integer(), db.ForeignKey(Shirt.id), primary_key=True)
-    shirt = db.relationship(Shirt, backref='photos')
-
-Shirt.has_photo = db.column_property(db.exists(['*']).where(Shirt.id == ShirtPhoto.shirt_id))
-
+@with_notes
+@with_photos
+@with_pending_photos
 class Wearing(db.Model):
     __tablename__ = 'wearings'
     id = db.Column(db.Integer(), primary_key=True)
@@ -100,22 +115,6 @@ class Wearing(db.Model):
     shirt = db.relationship(Shirt, backref='wearings')
     when = db.Column(db.Date(), nullable=False, index=True)
     specifically_when = db.Column(db.Time())
-
-class WearingNote(Note):
-    __tablename__ = 'wearing_notes'
-    __mapper_args__ = dict(polymorphic_identity='wearing')
-    note_id = db.Column(db.Integer(), db.ForeignKey(Note.id), primary_key=True)
-    wearing_id = db.Column(db.Integer(), db.ForeignKey(Wearing.id), primary_key=True)
-    wearing = db.relationship(Wearing, backref='notes')
-
-class WearingPhoto(Photo):
-    __tablename__ = 'wearing_photos'
-    __mapper_args__ = dict(polymorphic_identity='wearing')
-    photo_id = db.Column(db.Integer(), db.ForeignKey(Photo.id), primary_key=True)
-    wearing_id = db.Column(db.Integer(), db.ForeignKey(Wearing.id), primary_key=True)
-    wearing = db.relationship(Wearing, backref='photos')
-
-Wearing.has_photo = db.column_property(db.exists(['*']).where(Wearing.id == WearingPhoto.wearing_id))
 
 @app.before_request
 def lookup_current_user():
@@ -195,7 +194,7 @@ def wear_on(day, month, year):
                           when=datetime.date(year, month, day))
         db.session.add(wearing)
         if form.description.data:
-            note = WearingNote(note=form.description.data, wearing=wearing)
+            note = Wearing.Note(note=form.description.data, wearing=wearing)
             db.session.add(note)
         db.session.commit()
         return redirect(url_for('wearing_calendar', month=month, year=year))
@@ -217,7 +216,7 @@ def photo_note(id):
     photo = Photo.query.get(id)
     form = AddNoteForm()
     if form.validate_on_submit():
-        note = PhotoNote(photo=photo, note=form.note.data)
+        note = Photo.Note(photo=photo, note=form.note.data)
         db.session.add(note)
         db.session.commit()
         return redirect(url_for('photo_detail', id=id))
@@ -239,11 +238,11 @@ def wearing_note(id):
     form = AddPhotoNoteForm()
     if form.validate_on_submit():
         if request.files.get('photo'):
-            photo = WearingPhoto(filename=photo_set.save(request.files['photo']), wearing=wearing)
-            note = PhotoNote(note=form.note.data, photo=photo)
+            photo = Wearing.Photo(filename=photo_set.save(request.files['photo']), wearing=wearing)
+            note = Photo.Note(note=form.note.data, photo=photo)
             db.session.add_all([photo, note])
         else:
-            note = WearingNote(wearing=wearing, note=form.note.data)
+            note = Wearing.Note(wearing=wearing, note=form.note.data)
             db.session.add(note)
         db.session.commit()
         return redirect(url_for('wearing_detail', id=id))
@@ -266,11 +265,11 @@ def shirt_note(id):
     form = AddPhotoNoteForm()
     if form.validate_on_submit():
         if request.files.get('photo'):
-            photo = ShirtPhoto(filename=photo_set.save(request.files['photo']), shirt=shirt)
-            note = PhotoNote(note=form.note.data, photo=photo)
+            photo = Shirt.Photo(filename=photo_set.save(request.files['photo']), shirt=shirt)
+            note = Photo.Note(note=form.note.data, photo=photo)
             db.session.add_all([photo, note])
         else:
-            note = ShirtNote(shirt=shirt, note=form.note.data)
+            note = Shirt.Note(shirt=shirt, note=form.note.data)
             db.session.add(note)
         db.session.commit()
         return redirect(url_for('shirt_detail', id=id))
@@ -304,10 +303,10 @@ def shirt_add():
             acquired=form.acquired.data)
         db.session.add(shirt)
         if request.files.get('photo'):
-            photo = ShirtPhoto(filename=photo_set.save(request.files['photo']), shirt=shirt)
+            photo = Shirt.Photo(filename=photo_set.save(request.files['photo']), shirt=shirt)
             db.session.add(photo)
         if form.description.data:
-            note = ShirtNote(note=form.description.data, shirt=shirt)
+            note = Shirt.Note(note=form.description.data, shirt=shirt)
             db.session.add(note)
         db.session.commit()
         return redirect(url_for('shirts'))
